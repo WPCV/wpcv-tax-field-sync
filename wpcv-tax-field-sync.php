521dc3ce9d8eafc5d5d97116485cad6b0fa1c611
Christian Wach <needle@haystack.co.uk>
2025-08-09 16:44:21 +0100

    Bump PHP version

--- wpcv-tax-field-sync.php
+++ wpcv-tax-field-sync.php
@@ -10,7 +10,7 @@
  * Author:            WPCV
  * Author URI:        https://github.com/WPCV
  * Requires at least: 5.7
- * Requires PHP:      7.3
+ * Requires PHP:      7.4
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Text Domain:       wpcv-tax-field-sync
